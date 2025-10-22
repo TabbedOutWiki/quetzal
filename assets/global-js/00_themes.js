6a666b64cde941419f6b457b396c4deb0e4e98a4
maruhop <chad5409@gmail.com>
2025-10-22 16:59:04 -0700

    Changed theme to reduce duplicate CSS

--- assets/global-js/00_themes.js
+++ assets/global-js/00_themes.js
@@ -1,31 +1,28 @@
 (function() {
-  const THEME_KEY = 'theme'
   const select = document.querySelector('select#theme-switcher.menu');
 
-  const isValidTheme = (theme) => {
-    switch (theme) {
-      case 'light dark':
-      case 'light':
-      case 'dark':
-        return true;
-    }
+  // Check if global theme enum contains theme
+  const isValidTheme = (_theme) => {
+    if (Object.values(THEME).includes(_theme)) return true;
     return false;
   }
 
+  // On select change, match and set theme
   const handleChange = () => {
     const selectedOption = select.options[select.selectedIndex];
     const theme = selectedOption.value;
 
     if (isValidTheme(theme)) {
-      document.documentElement.dataset.theme = theme;
+      document.documentElement.className = theme;
       localStorage.setItem(THEME_KEY, theme);
     }
   }
 
-  const theme = localStorage.getItem(THEME_KEY) || 'light dark';
-  if (isValidTheme(theme)) {
+  // Detect theme and match select menu to it
+  const detectedTheme = localStorage.getItem(THEME_KEY) || THEME.DEFAULT;
+  if (isValidTheme(detectedTheme)) {
     for (const option of select.options) {
-      if (option.value === theme) option.selected = true;
+      if (option.value === detectedTheme) option.selected = true;
     }
   }
   
